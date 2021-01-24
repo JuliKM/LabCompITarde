4921a701b32eee19ac3ce8afade7f649b4014244
JuliKM <57647619+JuliKM@users.noreply.github.com>
2021-01-24 12:58:43 -0300

    Update Ej_3.java

--- TPColecciones/Ej_3.java
+++ TPColecciones/Ej_3.java
@@ -6,43 +6,56 @@
  * promedio aritmético d) cuántos de los números son menores que el promedio
  * aritmético
  */
-package TPCollecciones;
+package colecciones1;
 
 import java.util.ArrayList;
 
 public class Ej_3 {
+	public static void main(String[] args) {
+		// (int)(Math.random() * maximum);
+		ArrayList<Integer> arr = new ArrayList();
 
-    public static void main(String[] args) {
-        //(int)(Math.random() * maximum);
-        ArrayList<Integer> arr = new ArrayList();
+		ArrayList<Integer> mayores = new ArrayList();
+		ArrayList<Integer> menores = new ArrayList();
+		ArrayList<Integer> iguales = new ArrayList();
 
-        ArrayList<Integer> mayores = new ArrayList();
-        ArrayList<Integer> menores = new ArrayList();
-        
-        for (int i = 0; i < 20; i++) {
-            arr.add((int) (Math.random() * 100));
-        }
-        Double suma = 0.0;
-        int mayor = 0;
-        int menor = 0;
-        for (Integer arr1 : arr) {
-            System.out.print(". Nro: " + arr1);
-            suma += arr1;
-        }
-        Double promedio = suma / 20;
-        for (Integer menore : arr) {
-            if (menore < promedio) {
-                menores.add(menore);
-            mayor++;
-            }
-        }
-        for (Integer mayore : arr) {
-            if (mayore > promedio) {
-                menores.add(mayore);
-            menor++;
-            }
+		for (int i = 0; i < 20; i++) {
+			arr.add((int) (Math.random() * 100));
+		}
+		Double suma = 0.0;
+		int mayor = 0;
+		int menor = 0;
+		int igual = 0;
 
-        }
-        System.out.println("Promedio: "+promedio+"\n Hay "+menor+" numeros menores al promedio\n y "+mayor+" numeros mayores al promedio");
-    }
+		for (Integer arr1 : arr) {
+			System.out.print("Nro: " + arr1 + "\t ");
+			suma += arr1;
+		}
+		Double promedio = suma / 20;
+		for (Integer menore : arr) {
+			if (menore < promedio) {
+				menores.add(menore);
+				mayor++;
+			}
+		}
+		int promedioInt = (int) Math.round(promedio);
+		for (Integer mayore : arr) {
+			if (mayore > promedio) {
+				mayores.add(mayore);
+				menor++;
+			}
+
+		}
+		for (Integer iguale : arr) {
+
+			if (iguale == promedioInt) {
+				iguales.add(iguale);
+				igual++;
+			}
+		}
+		System.out.println("\nPromedio redondeado: " + promedioInt + ". Hay " + igual + " numeros iguales al promedio");
+		System.out.println("\nPromedio: " + promedio + "\nHay " + menor + " numeros menores al promedio y " + mayor
+				+ " numeros mayores al promedio");
+
+	}
 }
